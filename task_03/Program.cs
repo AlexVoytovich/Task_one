671fab58e2d82aca04acf2d4893add444fafcaa8
AlexVoyt <alexvoytovich@bk.ru>
2022-10-07 11:30:15 +0500

    Задание 3

---
+++ task_03/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число для проверки его на четность: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int ost = a % 2;
+if (ost == 0)
+    Console.WriteLine("Число четное");
+else
+    Console.WriteLine("Число не четное");
+
