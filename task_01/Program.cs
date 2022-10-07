696c98b4e73eb670e38e4d50ba3610cfc52cc0fc
AlexVoyt <alexvoytovich@bk.ru>
2022-10-07 10:46:58 +0500

    Первая задача

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿Console.WriteLine("Введите два числа для их сравнения: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine("Max = a");
+else
+    Console.WriteLine("max = b");
